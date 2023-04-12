b4f78e59a9c6bd9f371673089dce21c66255d341
fardeeba <Farah.Deeba@afiniti.com>
2023-04-13 00:13:54 +0500

    change_3

--- trustedseg.py
+++ trustedseg.py
@@ -97,7 +97,7 @@ class TMSU(nn.Module):
         :return: evidence of modal data
         """
         # evidence = (input-torch.min(input))/(torch.max(input)-torch.min(input))
-        evidence = nn.softmax(input)
+        evidence = nn.Softmax(input)
         # evidence[m_num] = torch.exp(torch.clamp(evidence, -10, 10))
         # evidence = F.relu(evidence)
         return evidence
