d21a1d169e956279fdd9db6e18db6a8996d2b97f
fardeeba <Farah.Deeba@afiniti.com>
2023-04-28 13:55:43 +0500

    print losses at each step

--- trainTBraTS.py
+++ trainTBraTS.py
@@ -368,7 +368,7 @@ if __name__ == "__main__":
             print("epoch %d avg_enhancing_tumor_iou_score:%0.3f" % (epoch, avg_enhancing_iou))
             training_losses.append(epoch_loss.detach().cpu().numpy())
             validation_losses.append(val_loss.detach().cpu().numpy())
-            if epoch % 5 == 0:
+            if epoch % 1 == 0:
                 plt.plot(training_losses, label='Training loss')
                 plt.plot(validation_losses, label='Validation loss')
                 plt.xlabel('Epoch')
